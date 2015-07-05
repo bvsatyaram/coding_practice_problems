c36560b5d8e547783c3b9abb49d5c9c446cca9c0
BV Satyaram <bvsatyaram@gmail.com>
2015-07-05 19:00:07 +0530

    Adding 'Fill matrix with zeros' problem statement

---
+++ 11_fill_matrix_with_zeros.rb
@@ -0,0 +1,20 @@
+# The file 'data/binary_matrix.txt' has a 2D matrix.
+# The matrix has only 0s and 1s.
+# Write a program that transforms the matrix as follows:
+# If an element of the matrix is zero, then it's entire row
+# and column should be set to zero.
+
+matrix = []
+20.times do
+  row = []
+  20.times do
+    if rand(40) == 0
+      row.push(0)
+    else
+      row.push(1)
+    end
+  end
+  matrix.push(row)
+end
+
+puts matrix.collect{|row| row.join(" ")}.join("\n")
