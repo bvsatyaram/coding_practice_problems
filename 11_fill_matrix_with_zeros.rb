343e449c3a79bdc1fab6d13fd48dff646bc54401
BV Satyaram <bvsatyaram@gmail.com>
2015-07-05 20:08:25 +0530

    Updating problem statement

--- 11_fill_matrix_with_zeros.rb
+++ 11_fill_matrix_with_zeros.rb
@@ -4,17 +4,20 @@
 # If an element of the matrix is zero, then it's entire row
 # and column should be set to zero.
 
-matrix = []
-20.times do
-  row = []
-  20.times do
-    if rand(40) == 0
-      row.push(0)
-    else
-      row.push(1)
-    end
-  end
-  matrix.push(row)
-end
+# The following code is used to generate the random matrix
+# 
+# matrix = []
+# 20.times do
+#   row = []
+#   20.times do
+#     if rand(40) == 0
+#       row.push(0)
+#     else
+#       row.push(1)
+#     end
+#   end
+#   matrix.push(row)
+# end
+# 
+# puts matrix.collect{|row| row.join(" ")}.join("\n")
 
-puts matrix.collect{|row| row.join(" ")}.join("\n")
