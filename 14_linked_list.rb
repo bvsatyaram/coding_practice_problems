8c3a1855432d60f46650bac0fa1972b24dd78fb7
BV Satyaram <bvsatyaram@gmail.com>
2015-07-07 14:49:53 +0530

    Implementing Delete for linked lists

--- 14_linked_list.rb
+++ 14_linked_list.rb
@@ -51,6 +51,48 @@ class LinkedList
 
     return counter
   end
+
+  def nth_element(n)
+    element = @sentinel
+    counter = 0
+    while counter < n
+      element = element.next
+      counter += 1
+    end
+
+    return element
+  end
+
+  # +val+ is the value of the node to be inserted
+  # +ind+ is the index at which the node should be inserted
+  # Sentinel has index 0 and head has index 1.
+  def insert(val, index)
+    node = LinkedListNode.new(val)
+    prev_ele = self.nth_element(index - 1)
+    next_ele = prev_ele.next
+    node.next = next_ele
+    prev_ele.next = node
+  end
+
+  def delete(index)
+    prev_element = self.nth_element(index - 1)
+    element_to_delete = prev_element.next
+    prev_element.next = element_to_delete.next
+    # Release the deleted node, so that GC can collect it soon
+    element_to_delete.next = nil
+  end
+
+  def print
+    element = head
+    str = ""
+    while !element.nil?
+      str += element.value.to_s
+      str += " -> "
+      element = element.next
+    end
+
+    puts str
+  end
 end
 
 ll = LinkedList.new
@@ -64,6 +106,8 @@ ll.prepend(4)
 ll.head.value # 4
 ll.tail.value # 3
 ll.length # 4
-
-ll.insert(4, 3)
-# ll.delete(2)
+ll.print
+ll.insert(7, 3)
+ll.print
+ll.delete(2)
+ll.print
